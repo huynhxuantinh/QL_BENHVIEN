5715b3944aa6c69c2da5ef60ec3833723c8032e3
XuanTinh <afmzxje@gmail.com>
2026-02-02 00:15:48 +0700

    duma

--- core/urls.py
+++ core/urls.py
@@ -2,12 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('benh-vien/<int:id>/', views.hospital_detail, name='hospital_detail'),
-    path("dat-lich/", views.dat_lich, name="dat_lich"),
-        path("login/", views.user_login, name="login"),
+
+    # Trang chủ
+    path("", views.home, name="home"),
+
+    # Chi tiết bệnh viện
+    path("benh-vien/<int:id>/", views.hospital_detail, name="hospital_detail"),
+
+    # Đặt lịch theo bệnh viện
+    path("dat-lich/<int:bv_id>/", views.dat_lich, name="dat_lich"),
+
+    # Auth
+    path("login/", views.user_login, name="login"),
     path("register/", views.register, name="register"),
     path("logout/", views.user_logout, name="logout"),
-    path("dat-lich/<int:bv_id>/", views.dat_lich, name="dat_lich"),
 
 ]

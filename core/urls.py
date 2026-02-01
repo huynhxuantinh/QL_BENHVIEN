3a9c4940c0060bce4a840e5905935a177380955e
XuanTinh <afmzxje@gmail.com>
2026-02-02 01:03:10 +0700

    anh tày

--- core/urls.py
+++ core/urls.py
@@ -18,4 +18,5 @@ urlpatterns = [
     path("logout/", views.user_logout, name="logout"),
     path("bac-si/kham/<int:lich_id>/", views.doctor_exam, name="doctor_exam"),
     path("bac-si/", views.bac_si_home, name="bac_si_home"),
+    
 ]
